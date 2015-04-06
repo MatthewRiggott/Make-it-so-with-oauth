2c10632e90e398a4d0e7b1f282b6507b2314020b
Matthew Riggott <Matthew.S.Riggott@gmail.com>
2015-04-05 21:44:29 -0400

    first commit, added user authentication, fb google oauth

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'homes#index'
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+
+  # in case the oauth provider doesn't provide a verified email address
+  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+
+
+end
