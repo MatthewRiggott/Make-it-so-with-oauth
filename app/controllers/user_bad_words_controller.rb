1e2aac6f0a4f5f77d606b676640ff86e545851ae
Liz Morrison <elizabethmorrison@ELIZABETHs-MacBook-Pro.local>
2015-05-16 16:17:51 -0400

    Able to to add user bad words. Need to correct how collecting.

--- app/controllers/user_bad_words_controller.rb
+++ app/controllers/user_bad_words_controller.rb
@@ -1,20 +1,19 @@
 class UserBadWordsController < ApplicationController
   def create
-    binding.pry
     words_array = user_bad_word_params[:bad_words].downcase.split(",").map do |word|
-      BadWord.where(word: word.strip).find_or_create!
+      BadWord.find_or_create_by!(word: word.strip)
     end
     words_array.each do |word|
-      UserBadWord.create (user: current_user, word: BadWord.find(word: word))
+      binding.pry
+      UserBadWord.create(user: current_user, bad_word: word)
     end
     redirect_to bad_words_path
-    end
   end
 
   protected
 
   def user_bad_word_params
-    params.require(:user_bad_words).permit(:bad_words)
+    params.require(:user_bad_word).permit(:bad_words)
   end
 
 end
