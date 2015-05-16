baa9bd03ab3b44bcbb4788d8f713cdddea432e2e
Matthew Riggott <Matthew.S.Riggott@gmail.com>
2015-05-16 13:12:05 -0400

    syntax

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -29,7 +29,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV["FB_KEY"], ENV["FB_SECRET"], { scope: 'public_profile','email','user_status'}
+  config.omniauth :facebook, ENV["FB_KEY"], ENV["FB_SECRET"], { scope: 'public_profile,email,user_status'}
   config.omniauth :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"], { :scope => 'userinfo.email' }
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
